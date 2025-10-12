ca48429a0a9f8f234958018f0ea0a7467e87de90
Victor <victor.jesus@aluno.faculdadeimpacta.com.br>
2025-10-11 21:29:27 -0300

    BD Render online

--- EcoLumen/config.py
+++ EcoLumen/config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    DB_HOST = "dpg-d30rmj7diees7389bb50-a.virginia-postgres.render.com"
-    DB_NAME = "hotel_sustentavel"
+    DB_HOST = "dpg-d3lfa6ogjchc73cc9j90-a.virginia-postgres.render.com"
+    DB_NAME = "hotel_sustentavel_fyw4"
     DB_USER = "root"
-    DB_PASSWORD = "7SaJncuFQpFZvsL7bO8v3dS3zJc2accC"
+    DB_PASSWORD = "yDRGhuENnf7CmBIJdVK0Quvu06iAx7PE"
     DB_PORT = "5432"
