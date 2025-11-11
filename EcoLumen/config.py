113e83b201d1a7199f88a5c3e29fa1b5cf5c410c
Victor <victor.jesus@aluno.faculdadeimpacta.com.br>
2025-11-11 09:13:22 -0300

    Banco de dados no Ar

--- EcoLumen/config.py
+++ EcoLumen/config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    DB_HOST = "dpg-d3lfa6ogjchc73cc9j90-a.virginia-postgres.render.com"
-    DB_NAME = "hotel_sustentavel_fyw4"
+    DB_HOST = "dpg-d49ifindiees73agk1v0-a.virginia-postgres.render.com"
+    DB_NAME = "ecolumen"
     DB_USER = "root"
-    DB_PASSWORD = "yDRGhuENnf7CmBIJdVK0Quvu06iAx7PE"
+    DB_PASSWORD = "ucwSuki97fhEcWYgRdnHKApoLjkSdOnZ"
     DB_PORT = "5432"
